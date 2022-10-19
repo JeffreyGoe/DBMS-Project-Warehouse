bda5d1a4cc5029bc16e02bd25cb5d9b0d1cbf432
jeffrey <goerkeje@gmail.com>
2022-10-19 17:26:14 -0500

    Intial Commit

---
+++ db.py
@@ -0,0 +1,33 @@
+#Create the tables
+
+
+def create_customer_table():
+    import sqlite3
+    connect = sqlite3.connect('warehouse.db')
+    cur = connect.cursor()
+    #creates database query
+    cur.execute("""CREATE TABLE IF NOT EXISTS Customer (
+                username TEXT UNIQUE PRIMARY KEY,
+                password TEXT,
+                birthday TEXT)
+                """)
+    #commit and close the connection
+    connect.commit()
+    connect.close()
+
+
+def create_items_table():
+    import sqlite3
+    connect = sqlite3.connect('warehouse.db')
+    cur = connect.cursor()
+    #creates database query
+    cur.execute("""CREATE TABLE IF NOT EXISTS ITEMS (
+                username TEXT UNIQUE PRIMARY KEY,
+                password TEXT,
+                birthday TEXT)
+                """)
+    #commit and close the connection
+    connect.commit()
+    connect.close()
+
+    
